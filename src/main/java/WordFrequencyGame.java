dae630bac6b14d1a1eab268220ae76739f209506
Eason <chenzscd@163.com>
2020-07-27 15:53:56 +0800

    [refactor][Eason] refactor: rename map to wordInfoMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -28,10 +28,10 @@ public class WordFrequencyGame {
                     wordInfos.add(wordInfo);
                 }
 
-                Map<String, List<WordInfo>> map =getListMap(wordInfos);
+                Map<String, List<WordInfo>> wordInfoMap =getListMap(wordInfos);
 
                 List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
+                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()) {
                     WordInfo input = new WordInfo(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
